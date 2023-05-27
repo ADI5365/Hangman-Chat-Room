1243a7ba2f068d564af6b7eea5c59be2631a9091
Andrea <adirwin23@gmail.com>
2023-05-27 15:25:43 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,3 @@
+# Author: Andrea Irwin
+# Class: CS 372-400 SP23
+# Client-side of the chat/game programming project
---
+++ server.py
@@ -0,0 +1,3 @@
+# Author: Andrea Irwin
+# Class: CS 372-400 SP23
+# Server-side of the chat/game programming project
