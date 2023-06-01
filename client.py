7cc6151820f42edebec3ed448da2a960f6a06ecc
Andrea <adirwin23@gmail.com>
2023-06-01 14:18:57 -0700

    Chat room functionality fully set up

--- client.py
+++ client.py
@@ -2,6 +2,11 @@
 # Class: CS 372-400 SP23
 # Client-side of the chat/game programming project
 
+# Citation for chat room environment set up:
+# Date: 6/1/23
+# Adapted from CS 372 Project 1 server and client environment set up
+# and from https://www.biob.in/2018/04/simple-server-and-client-chat-using.html
+
 
 import time
 import socket
@@ -14,40 +19,53 @@ def setUpChat_Client():
 
     Sets up an instance of a client-side access to a chat with a server
     """
-    print("\nWelcome to the [Game] Chat Room\n")
-    print("Initialising....\n")
+    print('\nWelcome to the [Game] Chat Room\n')
+    print('Initializing....\n')
     time.sleep(1)
 
+    # # Set up the client socket tand data that sends to server
     client_socket = socket.socket()
     socket_host = socket.gethostname()
     ip = socket.gethostbyname(socket_host)
     print(socket_host, "(", ip, ")\n")
 
-    host_address = input(str("Enter server address: "))
-    name = input(str("\nEnter your name: "))
-    port = 4230
-    print("\nTrying to connect to ", host_address, "(", port, ")\n")
+    # Select the address of the server to connect to in chat
+    host_address = input(str('Enter server address: '))
+    name = input(str('\nEnter your name: '))
+    port = 3120
 
-    time.sleep(1)
-    client_socket.connect((host_address, port))
-    print("Connected...\n")
+    # Bind the port number to this socket
+    # Set up to connect to the serverr's chat room
+    try: 
+        print('\nTrying to connect to ', host_address, '(', port, ')\n')
+        time.sleep(1)
+        client_socket.connect((host_address, port))
+        print('Connected...\n')
+    except:
+        print('Error: socket failed to launch')
 
-    client_socket.send(name.encode())
-    user_name = client_socket.recv(4096)
-    user_name = user_name.decode()
-    print(user_name, "has joined the chat room\nEnter [e] to exit chat room\n")
+    # Put in client username and enter the chat room
+    try:
+        client_socket.send(name.encode())
+        user_name = client_socket.recv(1024)
+        user_name = user_name.decode()
+        print(user_name, 'has joined the chat room\nEnter [e] to exit chat room\n')
+    except:
+        print('Error: failed to connect')
 
+    # As long as the chat room is open, the server and client take turns
+    # sending messages back and forth
     while True:
-        message = client_socket.recv(4096)
+        message = client_socket.recv(1024)  # Server incoming messages
         message = message.decode()
-        print(user_name, ":", message)
-        message = input(str("Me : "))
-        if message == "[e]":
-            message = "Left chat room!"
+        print(user_name, ':', message)
+        message = input(str('Me : '))
+        if message == '[e]':
+            message = 'Left chat room'
             client_socket.send(message.encode())
-            print("\n")
+            print('\n')
             break
-        client_socket.send(message.encode())
+        client_socket.send(message.encode())  # Sending back a message to server
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -2,6 +2,9 @@
 # Class: CS 372-400 SP23
 # Server-side of the chat/game programming project
 
+# Basic server/client chat environment set up adapted from BioGem
+# Source URL: https://www.biob.in/2018/04/simple-server-and-client-chat-using.html
+
 
 import time
 import socket
@@ -14,39 +17,54 @@ def setUpChat_Server():
 
     Sets up an instance of a server-side access to a chat with a client
     """
-    print("\nWelcome to Chat Room\n")
-    print("Initialising....\n")
+    print('\nWelcome to the [Game] chat room\n')
+    print('Initializing....\n')
     time.sleep(1)
 
-    s = socket.socket()
-    host = socket.gethostname()
-    ip = socket.gethostbyname(host)
-    port = 3020
-    s.bind((host, port))
-    print(host, "(", ip, ")\n")
-    name = input(str("Enter your name: "))
+    # Set up the server socket and data to send to clients
+    serverSocket = socket.socket()
+    socketHost = socket.gethostname()
+    ip = socket.gethostbyname(socketHost)
+    serverPort = 3120
+    
+    # Bind the port number to this socket
+    # Set up to listen for client requests to connect to the chat
+    try:
+        serverSocket.bind((socketHost, serverPort))
+        print(socketHost, '(', ip, ')\n')
+        clientName = input(str('Enter your name: '))
 
-    s.listen(1)
-    print("\nWaiting for incoming connections...\n")
-    conn, addr = s.accept()
-    print("Received connection from ", addr[0], "(", addr[1], ")\n")
+        serverSocket.listen(1)
+        print('\nWaiting for client requests to connect...\n')
+        connSocket, addr = serverSocket.accept()
+        print('Received connection from ', addr[0], '(', addr[1], ')\n')
+    except:
+        print('Error: socket failed to launch')
 
-    s_name = conn.recv(4096)
-    s_name = s_name.decode()
-    print(s_name, "has connected to the chat room\nEnter [e] to exit chat room\n")
-    conn.send(name.encode())
+    # When the client sets up the chat environment on their side 
+    # the connection is established
+    try:
+        socketName = connSocket.recv(1024)
+        socketName = socketName.decode()
+        print(socketName,
+              'has connected to the chat room\nEnter [e] to exit chat room\n')
+        connSocket.send(clientName.encode())
+    except:
+        print('Error: client has failed to connect')
 
+    # As long as the chat room is open, the server and client take turns
+    # sending messages back and forth
     while True:
-        message = input(str("Me : "))
-        if message == "[e]":
-            message = "Left chat room!"
-            conn.send(message.encode())
-            print("\n")
+        message = input(str('Me : '))  
+        if message == '[e]':
+            message = 'Left chat room'
+            connSocket.send(message.encode())
+            print('\n')
             break
-        conn.send(message.encode())
-        message = conn.recv(4096)
+        connSocket.send(message.encode())  # Sending message to client
+        message = connSocket.recv(1024)
         message = message.decode()
-        print(s_name, ":", message)
+        print(socketName, ':', message)  # Client incoming messages
 
 
 if __name__ == '__main__':
